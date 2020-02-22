038b3e6be390f6fd13003afc8c5b13c688b7fb5a
lsmith <lsmith@msxi-euro.com>
2020-02-22 18:38:27 +0000

    Add unit tests for the persistence API implementation on the transformer. See MER-1828 / MER-1793
    * Added test to save model
    * Added test to load saved model and ensure the lookup tables are loaded correctly
    
    # Conflicts:
    #	src/test/scala/com/msxi/mwise/autopay/TestUtils.scala

---
+++ src/test/scala/com/msxi/mwise/autopay/TestUtils.scala
@@ -0,0 +1,24 @@
+package com.msxi.mwise.autopay
+
+import java.io.File
+
+/**
+  * <br> <br> 
+  * Copyright:    Copyright (c) 2019 <br> 
+  * Company:      MSX-International  <br>
+  *
+  * @author Laurence Smith
+  */
+object TestUtils {
+
+  implicit def getFileAsString(fileName: String) = {
+    val stream = getClass.getClassLoader.getResourceAsStream(fileName)
+    val source = scala.io.Source.fromInputStream(stream)
+    try source.mkString finally source.close()
+  }
+
+  def getTestResourceDir: File = {
+    val sampleUrl = getClass.getClassLoader.getResource("sample_claims.csv")
+    new File(sampleUrl.getFile).getParentFile
+  }
+}
